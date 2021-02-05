49be6070317cfaf607bbf892dfe3b47a1a2cd020
Korotov-Sketch <olegkorotof@mail.ru>
2021-02-05 13:07:23 +0400

    Little change

--- src/main/java/Decodonator.java
+++ src/main/java/Decodonator.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-public class Decoder {
+public class Decodonator {
     public static void main(String[] args) throws Exception {
         FileReader fileReader = new FileReader("src/main/resources/TextWithWord.txt");
         BufferedReader readerTextWithWord = new BufferedReader(fileReader);
